8e39f38d515e68cf09dc14736148f31005fc4c3a
Zlubia <143330008+Zlubia@users.noreply.github.com>
2024-04-10 21:35:33 +0200

    Update setlist.py
    - Recuperé l'ID (mbid) de l'artiste.
    - Récuperé la première page de setlist (20 resultats)
    
    Next :
    - Trier les setlist
    - Commencer avec l'API youtube - voir ce qu'il y aura besoin comme infos.

--- setlist.py
+++ setlist.py
@@ -1,14 +1,20 @@
 from repertorio import Repertorio
 
-api = Repertorio('API key')
+api = Repertorio('')
 
-ArtistSearch = input("Which artist would you like to search for ?")
+ArtistName = input("Which artist would you like to search for ?")
 
-Artist = api.artists(artistName=ArtistSearch, sort='relevance')
-#Setlist = api.setlists(artistName=ArtistSearch, year='2024')
+ArtistSearch = api.artists(artistName=ArtistName, sort='relevance')
+
+
+"""----- ADD a validation step here, if the first one in the list is not ok, check the second,etc..."""
+Artist = ArtistSearch['artist'][0] #[0] is the first result of the search
+ArtistMbid = Artist["mbid"]
 
 print(Artist)
 
 print("------")
 
-#print(Setlist)
+Setlist = api.artist_setlists(ArtistMbid)
+
+print(Setlist["setlist"])
